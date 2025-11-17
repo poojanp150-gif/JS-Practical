29785c9672267757a7d331fac0b77d3a1ded33a2
student-poojan <poojanp150@gmail.com>
2025-11-17 13:08:46 +0530

    first commit

---
+++ Assigment/Basic.js
@@ -0,0 +1,120 @@
+// 1. JavaScript Program To Print Hello World
+
+console.log("Hello World");
+
+
+// 2. JavaScript Program to Add Two Numbers
+
+let a = 40 , b = 20
+console.log(a+b)
+
+
+// 3. JavaScript Program to Find the Square Root
+var square = 25
+let ans = Math.sqrt(square)
+console.log(ans);
+
+
+// 4. JavaScript Program to Calculate the Area of a Rectangle
+let Length = 10, Width = 5
+let area = Length * Width
+console.log("Area of a Rectangle is: "+area);
+
+
+// 5. JavaScript Program to Swap Two Variables
+var x=40, y=10
+x = x + y
+y = x - y
+x = x - y
+console.log("Value of X is:"+x+" and Value of Y is:"+y);
+
+
+// 6. Javascript Program to Convert Celsius to Fahrenheit
+let Celsius = 25
+let Fahrenheit = Celsius * 9/5 + 32
+console.log("Fahrenheit "+ Fahrenheit);
+
+
+// 7. Simple Interest.
+var p = 1000, r = 5, n = 2
+const s = p*r*n/100
+console.log("Simple Interest is: "+s); 
+
+
+// 8. Write a program to create Simple Calculator using Operators.
+let num1 = 80, num2 = 40
+var Addition = num1 + num2
+var Subtraction = num1 - num2
+var Multiplication = num1 * num2
+var Division = num1 / num2
+
+console.log("Addition is: "+Addition);
+console.log("Subtraction is: "+Subtraction);
+console.log("Multiplication is: "+Multiplication);
+console.log("Division is: "+Division);
+
+
+// 9. Find the third angle of a right triangle if two other angles are given.
+let First_angle = 90, Second_angle = 30
+const Third_angle = 180 - (First_angle + Second_angle)
+console.log(Third_angle);
+
+
+// 10. Electricity Bill Calculation
+let unit = 250, rate = 6
+console.log("Your Electricity Bill is: "+unit*rate);
+
+
+// 11. Area of a Circle
+const PI = 3.14, radius = 7
+let area_of_circle = PI * radius * radius
+console.log("Area of a Circle is: "+area_of_circle);
+
+
+// 12. Cube of a Number
+let num_c = 4
+console.log("Cube: "+num_c*num_c*num_c);
+
+
+// 13. Employee Salary Slip
+let basic_Salary = 15000
+let hra = basic_Salary * 0.8
+let ta = basic_Salary * 0.09
+var gorss = basic_Salary + hra + ta
+console.log("Gorss Salary is: "+gorss);
+
+
+// 14. USD to INR (Assume $1 = ₹83)
+let USD = 43
+console.log("$"+USD +"= ₹"+USD*88.72 );
+
+
+// 15. Javascript Program to Convert Fahrenheit to Celsius
+let Fahrenheit_f = 104
+let Celsius_C = 5/9 * (Fahrenheit_f - 32)
+console.log(Celsius_C);
+
+
+// 16. Convert days to hours
+let days = 4
+console.log("Number of hour is: "+days*24);
+
+
+// 17. Convert kilometers to meters
+let kilometers = 2
+console.log("Number of Meters: "+ kilometers*1000);
+
+
+// 18. Convert Years to Month
+let year = 5
+console.log("Number of Month is: "+ year *12);
+
+
+// 19. Convert Minutes to Seconds
+let Minutes = 78
+console.log("Seconds is: "+Minutes*60);
+
+
+// 20. Convert Hours to Minutes
+let Hours = 78
+console.log("Minutes is: "+Hours*60);
